9fdcc59498ddc9aff40ea72d7bbe521d68c45299
Brown <browndw2@nih.gov>
2022-10-26 10:29:45 -0400

    Adding data set to NAMESPACE

---
+++ R/combined_data.R
@@ -0,0 +1,11 @@
+#' Simulated sample UK Biobank data including 30 individuals and 490 variables.
+#'
+#' A simulated dataset containing 30 individuals and 490 variables that can be used to test the functionality of the \code{tte} function. A detailed list of required variables are provided in the 'README_required_variables.txt' file.  
+#'
+#' @format A data frame with 30 rows and 490 variables.
+#'
+#' @examples
+#'   mydata <- readRDS("~/UKBBcleanR/data/combined_data.rds")
+#'
+#' @source {Machiela Lab}
+"combined_data"
